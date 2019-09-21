3914c1a70f524ebb67782b25921ee928024706e9
vlad19880508 <vlad19880508@gmail.com>
2019-09-21 21:40:22 +0300

    правки

--- Users.php
+++ Users.php
@@ -1,16 +1,13 @@
 <?php
-
 class Users
 {
-
     static private $countUsers = 0;
-
-
+	
     private $name;
     private $dateofBirthday;
     private $position;
     private $salary;
-
+	
     public function __construct($name, $dateofBirthday, $position, $salary)
     {
         $this->name = $name;
@@ -18,6 +15,7 @@ class Users
         $this->position = $position;
         $this->salary = $salary;
         self::$countUsers++; //self это обращения к статическим переменным
+		
     }
     // клонируем и обнуляем 2 параметра
     public function __clone()
@@ -26,33 +24,33 @@ class Users
         $this->salary = null;
         self::$countUsers++; //self это обращения к статическим переменным
     }
+	
     // задаем новый position
     public function setPosition($position)
     {
         $this->position = $position;
     }
-
+	
     public function getPosition($position)
     {
         return $this->position;
     }
-
-
+	
     public function setSalary($salary)
     {
         $this->salary = $salary;
     }
-
+	
     public function getSalary($salary)
     {
         return $this->salary;
     }
-
+	
     public static function getCountUser()
     {
         return self::$countUsers;
     }
-
+	
     public static function __callStatic($name, $arguments)
     {
         if ($name == 'setCountUser') {
@@ -60,12 +58,26 @@ class Users
         }
     }
 	
+	public function __sleep()
+	{
+		// для сериализации указываю 2 свойства:
+		$this->name = 'NewNmae'; // задаю имя
+		$this->dateofBirthday = '2010-10-10'; // задаю дату
+		return array('name', 'dateofBirthday', 'position', 'salary');
+	}
+	
 	public function __wakeup()
 	{
-		$this->name = 'empty';
-        $this->dateofBirthday = 'empty';
+		// при востановлении обьекта задать ему позицию: 'empty'
+		return $this->position = 'empty';
 	}
 	
 	
 }
+
+	
+	
+	
+	
+
 
